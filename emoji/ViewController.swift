c875e97747fb6f900b0eaad8c2e1d1ba8e1ef00b
Apple <apple@Apples-MacBook-Pro-2.local>
2019-07-11 16:16:19 -0700

    first save

--- emoji/ViewController.swift
+++ emoji/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    @IBAction func happyEmoji(_ sender: Any) {
+        let alertController = UIAlertController(title: "A positive message!", message: "Start each day with a grateful heart.", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func sadEmoji(_ sender: Any) {
+        let alertController =
+            UIAlertController(title: "A motivational message!", message: "Happiness comes in waves. It'll find you again.", preferredStyle:
+             UIAlertController.Style.alert)
+         
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
