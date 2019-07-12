0782dbffb9501c9b9ae8dead7edaa3d362444807
Apple <apple@Apples-MacBook-Pro-2.local>
2019-07-12 09:58:26 -0700

    first save

--- emoji/ViewController.swift
+++ emoji/ViewController.swift
@@ -10,21 +10,27 @@ import UIKit
 
 
 class ViewController: UIViewController {
+    //var emojis = "😕😀"
+    let emojis = ["😕": "Happiness comes in waves. It'll find you again.", "😀": "Start each day with a grateful heart."]
     
-    @IBAction func happyEmoji(_ sender: Any) {
-        let alertController = UIAlertController(title: "A positive message!", message: "Start each day with a grateful heart.", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        present(alertController, animated: true, completion: nil)
-    }
     
-    @IBAction func sadEmoji(_ sender: Any) {
-        let alertController =
-            UIAlertController(title: "A motivational message!", message: "Happiness comes in waves. It'll find you again.", preferredStyle:
-             UIAlertController.Style.alert)
-         
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        present(alertController, animated: true, completion: nil)
-    }
+    //    @IBAction func happyEmoji(_ sender: Any) {
+//        let alertController = UIAlertController(title: "A positive message!", message: "Start each day with a grateful heart.", preferredStyle: UIAlertController.Style.alert)
+    
+    @IBAction func showMessage(sender: UIButton) {
+            let selectedEmotion = sender.titleLabel?.text
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+    
+    //    @IBAction func sadEmoji(_ sender: Any) {
+//        let alertController =
+//            UIAlertController(title: "A motivational message!", message: "Happiness comes in waves. It'll find you again.", preferredStyle:
+//             UIAlertController.Style.alert)
+//
+//        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//        present(alertController, animated: true, completion: nil)
+//    }
 
         
     override func viewDidLoad() {
